1239e45e69e57f6353e4f3b37463c0fd5850ae56
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-07-04 08:42:11 -0300

    feat(models): :sparkles: Add user and recipe sqlalchemy table models

---
+++ backend/models/recipe.py
@@ -0,0 +1,11 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from database import Base
+
+
+class Recipe(Base):
+    __tablename__ = "recipes"
+
+    id_recipe: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
---
+++ backend/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+from database import Base
+from uuid import UUID, uuid4
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id_user: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+    username: Mapped[str] = mapped_column(String(50), unique=True)
+    fullname: Mapped[str] = mapped_column(String(100))
+    password_hash: Mapped[str] = mapped_column(String(100))
+    email: Mapped[str] = mapped_column(String(100))
+     
