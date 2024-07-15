479dd984e1c65a933807f15b5bb922f8302266d5
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-07-15 17:16:30 -0300

    refactor(pwd_hash): :recycle: Fix "hash" method at PasswordHasher.

--- backend/src/utils/password_hasher.py
+++ backend/src/utils/password_hasher.py
@@ -3,12 +3,13 @@ import bcrypt
 
 class PasswordHasher:
     @staticmethod
-    def hash(self, password_text: str) -> bytes:
-        return bcrypt.hashpw(password_text.encode("utf8", bcrypt.gensalt()))
+    def hash(password_text: str) -> bytes:
+        password_text = bytes(password_text, "utf-8")
+        return bcrypt.hashpw(password_text, bcrypt.gensalt())
 
     @staticmethod
-    def verify(self, password_text, password_hash) -> bool:
-        password_text = bytes(password_text)
-        password_hash = bytes(password_hash)
+    def verify(password_text, password_hash) -> bool:
+        password_text = bytes(password_text, "utf-8")
+        password_hash = bytes(password_hash, "utf-8")
         
         return bcrypt.checkpw(password_text, password_hash)
