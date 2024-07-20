f5db5654d5b7bc33735cce355c7a8645340373ed
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-07-20 14:56:46 -0300

    refactor: :art: Add UseCases as Injected Dependency to route functions.

--- backend/src/routers/user_router.py
+++ backend/src/routers/user_router.py
@@ -1,56 +1,33 @@
 from uuid import UUID
 
-from fastapi import APIRouter
-from src.use_cases.users.login_user_use_case import LoginUserUseCase
-from src.dtos.user_register_dto import UserRegisterDTO
-from src.dtos.user_login_dto import UserLoginDTO
-from src.dtos.user_dto import UserDTO
-from src.models.user import User
-from src.repositories.user_repository import UserRepository
-from src.use_cases.users.register_user_use_case import RegisterUserUseCase
-from src.use_cases.users.get_user_use_case import GetUserUseCase
-from src.use_cases.users.get_users_use_case import GetUsersUseCase
+from fastapi import APIRouter, Depends
+from ..dtos.user_register_dto import UserRegisterDTO
+from ..dtos.user_login_dto import UserLoginDTO
+from ..dtos.user_dto import UserDTO
+from ..use_cases.users.login_user_use_case import LoginUserUseCase
+from ..use_cases.users.register_user_use_case import RegisterUserUseCase
+from ..use_cases.users.get_user_use_case import GetUserUseCase
+from ..use_cases.users.get_users_use_case import GetUsersUseCase
 
-from fastapi.responses import JSONResponse
 
 user_router = APIRouter(prefix="/users", tags=["user"])
 
 
 @user_router.get("")
-async def get_all() -> list[UserDTO]:
-    repository = UserRepository()
-
-    use_case = GetUsersUseCase(repository)
-    users = use_case.execute()
-
-    return users
+async def get_all(use_case: GetUsersUseCase = Depends(GetUsersUseCase)) -> list[UserDTO]:
+    return use_case.execute()
 
 
 @user_router.get("/{id_user:uuid}")
-async def get(id_user: UUID) -> UserDTO:
-    repository = UserRepository()
-
-    use_case = GetUserUseCase(repository)
-    user = use_case.execute(id_user)
-
-    return {"user": user}
+async def get(id_user: UUID, use_case: GetUserUseCase = Depends(GetUserUseCase)) -> UserDTO:
+    return use_case.execute(id_user)
 
 
 @user_router.post("")
-async def register(user: UserRegisterDTO):
-    # O que geralmente eu retorno nessa rota?
-    repository = UserRepository()
+async def register(user: UserRegisterDTO, use_case: RegisterUserUseCase = Depends(RegisterUserUseCase)):
+    return use_case.execute(user)
 
-    use_case = RegisterUserUseCase(repository)
-    use_case.execute(user)
-
-    return JSONResponse(status_code=200, content={"message": "user added to database"})
 
 @user_router.post("/login")
-async def register(user: UserLoginDTO):
-    repository = UserRepository()
-
-    use_case = LoginUserUseCase(repository)
-    token = use_case.execute(user)
-
-    return JSONResponse(status_code=200, content={"token": token})
+async def login(user: UserLoginDTO, use_case: LoginUserUseCase = Depends(LoginUserUseCase)):
+    return use_case.execute(user)
