edb6b2e4bc96f704da65e8cd6d1e650337ec51db
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-07-20 14:55:18 -0300

    feat(dto): :sparkles: Add data validations (e.g password, fullname)

--- backend/src/dtos/user_register_dto.py
+++ backend/src/dtos/user_register_dto.py
@@ -1,10 +1,30 @@
 from typing import Union
-
-from pydantic import BaseModel
-
+from typing_extensions import Annotated
+from pydantic import BaseModel, field_validator, EmailStr, Field, validator
+import re
 
 class UserRegisterDTO(BaseModel):
-    fullname: str
-    email: str
-    username: str
-    password: str
+    fullname: Annotated[str, Field(min_length=3, max_length=50)]
+    email: Annotated[EmailStr, Field(max_length=64)]
+    username: Annotated[str, Field(max_length=32)]
+    password: Annotated[str, Field(min_length=8, max_length=64)]
+
+    @field_validator('fullname', mode="before")
+    @classmethod
+    def fullname_must_contain_space(cls, fullname: str) -> str:
+        if ' ' not in fullname:
+            raise ValueError('Fullname must contain at least one (1) space.')
+        return fullname.title()
+    
+    @field_validator('password')
+    @classmethod
+    def password_complexity(cls, value):
+        if not re.search(r'[A-Z]', value):
+            raise ValueError('A senha deve conter pelo menos uma letra maiúscula.')
+        if not re.search(r'[a-z]', value):
+            raise ValueError('A senha deve conter pelo menos uma letra minúscula.')
+        if not re.search(r'[0-9]', value):
+            raise ValueError('A senha deve conter pelo menos um número.')
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', value):
+            raise ValueError('A senha deve conter pelo menos um caractere especial.')
+        return value
