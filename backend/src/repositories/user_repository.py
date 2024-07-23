abbc5fb5e1782568327717568da843c2cc633fb3
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-07-23 18:15:22 -0300

    feat:

--- backend/src/repositories/user_repository.py
+++ backend/src/repositories/user_repository.py
@@ -5,14 +5,18 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 from ..models.user import User
 from ..utils.deps import get_db
+from ..utils.exceptions import UserAlreadyExistsError
 
 class UserRepository:
     def __init__(self, session: Annotated[Session, Depends(get_db)]) -> None:
         self.__session = session
 
     def add(self, user: User) -> None:
-            self.__session.add(user)
-            self.__session.commit()
+        if self.get_by_email(user.email) or self.get_by_username(user.username):
+            raise UserAlreadyExistsError()
+        
+        self.__session.add(user)
+        self.__session.commit()
 
     def all(self) -> list[User]:
         return self.__session.query(User).all()
@@ -21,4 +25,7 @@ class UserRepository:
         return self.__session.query(User).filter(User.id_user == id).first()
 
     def get_by_email(self, email: str) -> User | None:
-        return self._session.query(User).filter(User.email == email).first()
+        return self.__session.query(User).filter(User.email == email).first()
+    
+    def get_by_username(self, username: str) -> User | None:
+        return self.__session.query(User).filter(User.email == username).first()
