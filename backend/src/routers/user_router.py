e1dc5675e0f570753ae6488c1a7eedd4a329f56f
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-07-23 18:17:39 -0300

    refactor: :art: Standardize return response to routes.

--- backend/src/routers/user_router.py
+++ backend/src/routers/user_router.py
@@ -1,6 +1,7 @@
 from uuid import UUID
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import JSONResponse
 from ..dtos.user_register_dto import UserRegisterDTO
 from ..dtos.user_login_dto import UserLoginDTO
 from ..dtos.user_dto import UserDTO
@@ -8,26 +9,68 @@ from ..use_cases.users.login_user_use_case import LoginUserUseCase
 from ..use_cases.users.register_user_use_case import RegisterUserUseCase
 from ..use_cases.users.get_user_use_case import GetUserUseCase
 from ..use_cases.users.get_users_use_case import GetUsersUseCase
-
+from ..models.user import User
+from fastapi.encoders import jsonable_encoder
 
 user_router = APIRouter(prefix="/users", tags=["user"])
 
 
 @user_router.get("")
-async def get_all(use_case: GetUsersUseCase = Depends(GetUsersUseCase)) -> list[UserDTO]:
-    return use_case.execute()
-
+async def get_all(use_case: GetUsersUseCase = Depends(GetUsersUseCase)) -> JSONResponse:
+    users = use_case.execute()
+    
+    users = list(map(UserDTO, users))
+    
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={"users": jsonable_encoder(users)}
+    )
 
 @user_router.get("/{id_user:uuid}")
-async def get(id_user: UUID, use_case: GetUserUseCase = Depends(GetUserUseCase)) -> UserDTO:
-    return use_case.execute(id_user)
+async def get(id_user: UUID, use_case: GetUserUseCase = Depends(GetUserUseCase)) -> JSONResponse:
+    try:
+        
+        user = use_case.execute(id_user)
+
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={"users": jsonable_encoder(user)}
+        )
+    except Exception as e:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": str(e)}
+        )
 
 
 @user_router.post("")
-async def register(user: UserRegisterDTO, use_case: RegisterUserUseCase = Depends(RegisterUserUseCase)):
-    return use_case.execute(user)
-
+async def register(user: UserRegisterDTO, use_case: RegisterUserUseCase = Depends(RegisterUserUseCase)) -> JSONResponse:
+    try:
+        use_case.execute(user)
+        
+        return JSONResponse(
+            status_code=status.HTTP_201_CREATED,
+            content={"message": "User created"}
+        )
+    except Exception as e:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"message": str(e)}
+        )
 
 @user_router.post("/login")
-async def login(user: UserLoginDTO, use_case: LoginUserUseCase = Depends(LoginUserUseCase)):
-    return use_case.execute(user)
+async def login(user: UserLoginDTO, use_case: LoginUserUseCase = Depends(LoginUserUseCase)) -> JSONResponse:
+    try:
+        token = use_case.execute(user)
+        
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={
+                "message": "User authorized.",
+                "token": token}
+        )
+    except Exception as e:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"message": str(e)}
+        )
