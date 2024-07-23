fe89e526d315ec336f2ef73690e31e139d8cb87e
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-07-23 18:16:32 -0300

    refactor: :fire: Start recipes routes implementation.

--- backend/src/routers/recipe_router.py
+++ backend/src/routers/recipe_router.py
@@ -1,24 +1,10 @@
-from fastapi import APIRouter
-from src.dtos.recipes import Recipe
+from fastapi import APIRouter, Depends
 
-recipe_router = APIRouter(prefix="/recipes", tags=["recipes"])
+from ..utils.deps import get_authorization_token
+from ..dtos.recipes import Recipe
 
+router = APIRouter(prefix="/recipes", tags=["recipes"])
 
-@recipe_router.get("")
-async def get_recipes():
-    return {"response": "all recipes"}
-
-
-@recipe_router.post("")
-async def add_recipe(recipe: Recipe):
-    return recipe
-
-
-@recipe_router.put("{recipe_id}")
-async def update_recipe(recipe_id: int):
-    return {"response": f"updated recipe {recipe_id}"}
-
-
-@recipe_router.delete("{recipe_id}")
-async def delete_recipe(recipe_id: int):
-    return {"response": f"deleted recipe {recipe_id}"}
+@router.get("")
+async def get_all(token: str = Depends(get_authorization_token)):
+    return token
