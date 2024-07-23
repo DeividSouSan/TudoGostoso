3b71165cb6b82f616f6db0b59ee22b62eb89e754
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-07-23 17:19:16 -0300

    refactor: :recycle: Refactor User model with Roles feature and  unique and nullable constraints.

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -2,14 +2,20 @@ from uuid import UUID, uuid4
 
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
-from src.db.base import Base
+from ..db.base import Base
+import enum
 
 
+class UserRole(enum.Enum):
+    ADMIN = "admin"
+    USER = "user"
+    
 class User(Base):
     __tablename__ = "users"
 
-    id_user: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
-    username: Mapped[str] = mapped_column(String(50), unique=True)
-    fullname: Mapped[str] = mapped_column(String(100))
-    password_hash: Mapped[str] = mapped_column(String(100))
-    email: Mapped[str] = mapped_column(String(100))
+    id_user: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4, nullable=False)
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    fullname: Mapped[str] = mapped_column(String(100), nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    role: Mapped[UserRole] = mapped_column(nullable=False, default=UserRole.USER)
