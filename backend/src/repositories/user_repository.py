56d0ad8dd0f2418ab24659aa28516e96267cf1f4
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-07-23 17:20:03 -0300

    refactor: :recycle: Add Session as Depends.

--- backend/src/repositories/user_repository.py
+++ backend/src/repositories/user_repository.py
@@ -1,29 +1,24 @@
-from typing import Optional, TypeVar
+from typing import Optional, TypeVar, Annotated
 from uuid import UUID
 
+from fastapi import Depends
 from sqlalchemy.orm import Session
 from ..models.user import User
-
+from ..utils.deps import get_db
 
 class UserRepository:
-    def __init__(self):
-        from ..db.connection import engine
-
-        self.__engine = engine
+    def __init__(self, session: Annotated[Session, Depends(get_db)]) -> None:
+        self.__session = session
 
     def add(self, user: User) -> None:
-        with Session(self.__engine) as session:
-            session.add(user)
-            session.commit()
+            self.__session.add(user)
+            self.__session.commit()
 
     def all(self) -> list[User]:
-        with Session(self.__engine) as session:
-            return session.query(User).all()
+        return self.__session.query(User).all()
 
     def get_by_id(self, id: UUID) -> User | None:
-        with Session(self.__engine) as session:
-            return session.query(User).filter(User.id_user == id).first()
+        return self.__session.query(User).filter(User.id_user == id).first()
 
     def get_by_email(self, email: str) -> User | None:
-        with Session(self.__engine) as session:
-            return session.query(User).filter(User.email == email).first()
+        return self._session.query(User).filter(User.email == email).first()
