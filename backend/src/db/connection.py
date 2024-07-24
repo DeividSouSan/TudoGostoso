f3afae28c3be84976bc6514a67ca56999dfa3232
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-07-24 10:27:01 -0300

    refactor: :fire: Remove sessionmaker().

--- backend/src/db/connection.py
+++ backend/src/db/connection.py
@@ -2,11 +2,9 @@ import os
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
 URL_DATABASE = os.getenv("DATABASE_URL")
 
 engine = create_engine(URL_DATABASE)
-session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
