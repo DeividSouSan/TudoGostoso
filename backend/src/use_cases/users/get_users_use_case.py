07ed64eb8dd1439adf96728cdb873acd2d67a146
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-07-24 10:35:27 -0300

    refactor: :art: Improve cohesion of class, one responsibility only.

--- backend/src/use_cases/users/get_users_use_case.py
+++ backend/src/use_cases/users/get_users_use_case.py
@@ -1,14 +1,16 @@
+from fastapi.responses import JSONResponse
 from ...dtos.user_dto import UserDTO
 from ...repositories.user_repository import UserRepository
-from fastapi import Depends
+from fastapi import Depends, status
+from fastapi.encoders import jsonable_encoder
+from ...models.users import User
 
 class GetUsersUseCase:
     def __init__(self, repository: UserRepository = Depends(UserRepository)):
         self._repository = repository
 
-    def execute(self) -> list[UserDTO]:
-        users = self._repository.all()
+    def execute(self) -> list[User]:
+        return self._repository.all()
+        
 
-        users_dto = map(UserDTO, users)
-
-        return users_dto
+        
