982634a2d0cf1538dd9773fbdd628c235e65d09d
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-08-07 16:26:09 -0300

    refactor: Add Error Handling + switch "id" to "id_recipe"

--- api/src/use_cases/recipes/get_recipe.py
+++ api/src/use_cases/recipes/get_recipe.py
@@ -4,14 +4,20 @@ from fastapi import Depends
 
 from ...models.recipes import Recipe
 from ...repositories.recipe_repository import RecipeRepository
+from ...utils.exceptions import RecipeNotFound
 
 
 class GetRecipe:
     def __init__(self, repository: RecipeRepository = Depends(RecipeRepository)):
         self._repository = repository
 
-    def execute(self, id: UUID | str) -> Recipe:
-        if isinstance(id, str):
-            id = UUID(id)
+    def execute(self, id_recipe: UUID | str) -> Recipe:
+        if isinstance(id_recipe, str):
+            id_recipe = UUID(id_recipe)
 
-        return self._repository.get_by_id(id)
+        recipe = self._repository.get_by_id(id_recipe)
+
+        if not recipe:
+            raise RecipeNotFound
+
+        return recipe
