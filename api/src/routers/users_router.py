7e56403a0c256c5fb4b5e2fcae955731081f3eca
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-08-07 16:41:04 -0300

    refactor: :art: Add description to "/delete" route and rename "current_user" to "current_user_token".

--- api/src/routers/users_router.py
+++ api/src/routers/users_router.py
@@ -1,16 +1,15 @@
 from uuid import UUID
 
-from fastapi import APIRouter, Depends, HTTPException, Security, status
-from fastapi.encoders import jsonable_encoder
-from fastapi.params import Security
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Depends, HTTPException, status
 
+from api.src.utils.exceptions import UnauthorizedAccountDelete
+
+from ..use_cases.users.delete_user import DeleteUser
 from ..dtos.user.user_response_dto import UserResponseDTO
 from ..routers.auth_router import oauth2_scheme
 from ..use_cases.users.get_all_users import GetAllUsers
 from ..use_cases.users.get_user import GetUser
 from ..use_cases.users.search_user import SearchUser
-from ..utils.deps import get_authorization_token
 
 users_router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -19,39 +18,36 @@ users_router = APIRouter(prefix="/users", tags=["Users"])
     "",
     summary="Get all active users",
     description="Get all users that have an active account.",
+    dependencies=[Depends(oauth2_scheme)],
+    response_model=list[UserResponseDTO],
 )
 async def get_all(
-    use_case: GetAllUsers = Depends(GetAllUsers), token=Depends(oauth2_scheme)
+    use_case: GetAllUsers = Depends(GetAllUsers),
 ) -> dict:
 
     users = use_case.execute()
-
-    if users:
-        users = [UserResponseDTO(user) for user in users]
 
     return {"users": users}
 
 
 @users_router.get(
-    "/{id:uuid}",
+    "/{id_user:uuid}",
     summary="Get user by id",
     description="Get a user by its unique UUID id.",
+    response_model=UserResponseDTO,
+    dependencies=[Depends(oauth2_scheme)],
 )
 async def get(
-    id: UUID,
-    current_user: dict[str, str] = Depends(get_authorization_token),
+    id_user: UUID,
     use_case: GetUser = Depends(GetUser),
-    token=Depends(oauth2_scheme),
-) -> dict:
+) -> any:
 
-    user = use_case.execute(current_user["id"])
+    user = use_case.execute(id_user)
 
     if not user:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
         )
-
-    user = UserResponseDTO(user)
 
     return {"users": user}
 
@@ -60,12 +56,12 @@ async def get(
     "/search",
     summary="Search for users by parameters",
     description="Search for users by username or email.",
+    dependencies=[Depends(oauth2_scheme)],
+    response_model=dict[str, list[UserResponseDTO]],
 )
 async def search(
     username: str = "",
-    current_user: dict[str, str] = Depends(get_authorization_token),
     use_case: SearchUser = Depends(SearchUser),
-    token=Depends(oauth2_scheme),
 ) -> dict:
 
     users = use_case.execute(username)
@@ -76,12 +72,36 @@ async def search(
     return {"users": users}
 
 
-@users_router.delete("/{id:uuid}")
-async def delete(
-    username: str = "",
-    current_user: dict[str, str] = Depends(get_authorization_token),
-    use_case: SearchUser = Depends(SearchUser),
-    token=Depends(oauth2_scheme),
-) -> dict:
+@users_router.delete(
+    "/{id_user:uuid}",
+    summary="Delete a user from the database",
+    description="Delete a user by it's id.",
+    response_model=dict[str, list[UserResponseDTO]],
+    status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_200_OK: {
+            "description": "User account deleted."
 
-    pass
+        },
+        status.HTTP_403_FORBIDDEN: {
+            "description": "Unauthorized to delete account."
+        },
+    }
+)
+async def delete(
+    id_user: UUID,
+    current_user_token: dict[str, str] = Depends(oauth2_scheme),
+    use_case: DeleteUser = Depends(DeleteUser)
+) -> dict:
+    try:
+        use_case.execute(id_user, current_user_token)
+
+        return {
+            "message": "User deleted"
+        }
+
+    except UnauthorizedAccountDelete as e:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Unauthorized to delete account"
+        )
--- api/src/use_cases/users/delete_user.py
+++ api/src/use_cases/users/delete_user.py
@@ -11,15 +11,15 @@ class DeleteUser:
     def __init__(self, repository: UserRepository = Depends(UserRepository)) -> None:
         self._repository = repository
 
-    def execute(self, user_id: UUID, current_user: dict) -> None:
+    def execute(self, user_id: UUID, current_user_token: dict) -> None:
         user = self._repository.get_by_id(user_id)
 
         if not user:
             raise UserNotFound()
 
-        if current_user["role"] == "user":
+        if current_user_token["role"] == "user":
             TARGET_USER_ID = user.user_id
-            CURRENT_USER_ID = UUID(current_user["id"])
+            CURRENT_USER_ID = UUID(current_user_token["id"])
 
             if TARGET_USER_ID != CURRENT_USER_ID:
                 raise UnauthorizedAccountDelete()
