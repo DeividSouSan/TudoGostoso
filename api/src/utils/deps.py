6797fa6b67e5352f232b744196b82e022681fb13
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-08-13 06:52:35 -0300

    refactor: :recycle: Refactor get_authorization_token to be more generic and independent.

--- api/src/utils/deps.py
+++ api/src/utils/deps.py
@@ -1,16 +1,32 @@
 from typing import Generator
 
-from fastapi import Depends, Header, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import Depends, HTTPException, status
 from jose import jwt
 from sqlalchemy.orm import Session
 
-from api.src.dtos.user.user_login_request_dto import UserLoginRequestDTO
+from ..contracts.token_generator import ITokenGenerator
+
 
 from ..db.connection import engine
-from ..utils.token_generator import TokenGenerator
 
 
 def get_db() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
+
+
+def get_authorization_token(token: str, token_generator: ITokenGenerator) -> dict[str, str]:
+    try:
+        token_data = token_generator.verify(token)
+
+        return token_data
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token signature has expired.",
+        )
+    except jwt.JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="Token is invalid"
+        )
